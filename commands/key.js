813db524a116f17e43e536470f20008e4b04773d
timosdev99 <timothyisah4@gmail.com>
2024-09-03 01:46:01 +0100

    third commit

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,23 @@
+const keymanager = require('../lib/keymanager')
+const inquirer = require('inquirer')
+const color = require('colors')
+
+
 const key = {
-    set() {
-        console.log('set working')
+   async set() {
+        const key_manager = new keymanager()
+        const input = await inquirer.prompt([{
+            type: 'input',
+            name: 'key',
+            message: 'Enter API Key '.green + 'https://nomics.com',
+            validate: isRequired
+        }])
+        const key = key_manager.setKey(input.key)
+
+        if (key) {
+            console.log('Api key set' .blue)
+        }
+
     },
 
     show() {
